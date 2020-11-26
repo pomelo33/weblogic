5ceb6cc75a2df48a98e53e104096e5b1397c3ba8
pomelo <pomelo33@outlook.com>
2020-11-26 22:27:33 +0800

    update run.sh

--- add_t3_filter/run.sh
+++ add_t3_filter/run.sh
@@ -3,5 +3,5 @@
 passwd_dir="${HOME}/passwd"
 webligicBase="/weblogic/wlserver_12.2"
 sed -n '3,$p' ${passwd_dir} | while read line;do
-    var=$(${webligicBase}/wlserver/common/bin/wlst.sh /root/weblogic/add_rules.py $line > ${log_dir})
+    var=$(${webligicBase}/wlserver/common/bin/wlst.sh /root/weblogic/modify_rules.py $line > ${log_dir})
 done
